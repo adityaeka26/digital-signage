0dbc922073faaaa42aa90ceda688bf3cfccd9030
Aditya <aditya.eka409@gmail.com>
2019-02-01 09:45:01 +0700

    update favicon

--- application/views/V_dasbor.php
+++ application/views/V_dasbor.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title><?=$title?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="<?=base_url()?>assets/img/favicon2.png" title="favicon">
     <link rel="stylesheet" type="text/css" media="screen" href="<?=base_url()?>assets/css/bootstrap.min.css" />
     <link rel="stylesheet" type="text/css" media="screen" href="<?=base_url()?>assets/css/datatables.min.css" />
     <link rel="stylesheet" type="text/css" media="screen" href="<?=base_url()?>assets/css/style.css" />
